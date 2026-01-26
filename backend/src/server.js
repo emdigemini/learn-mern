ac1eebaee8449b89b936e4231dff29ed9f847cb8
Emdi Gemini <yougotjinx@gmail.com>
2026-01-26 10:39:55 +0800

    commit lol

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,18 +5,29 @@ import cors from "cors";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve();
 
 // middleware
-app.use(cors({
-  origin: "http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
+    origin: "http://localhost:5173",
+  }));
+};
 app.use(express.json()); // this middleware will parse the JSON bodies: allowing you to get access to body using req.body
 app.use(rateLimiter);
 
 app.use("/api/notes",  notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*path", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  })
+}
 
 (async () => {
   try {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+// kapag magkaparehas lang ng domain ang frontend at backend sapat na ang ganitong code for your BASE_URL
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+// kapag magkaiba naman sila ng domain dapat ganto
+// const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://yourwebsitename.com/api";
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api"
-})
+  baseURL: BASE_URL
+});
 
 export default axiosInstance;
